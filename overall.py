8fa44060579d9d0dd6f156ca93373fdbc674cd42
Meliora Ho <85920674+mellieho9@users.noreply.github.com>
2023-03-02 19:18:01 -0500

    Merged ratings and added logo to homepage

--- overall.py
+++ overall.py
@@ -1,51 +1,135 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 
-# Load the data from a CSV file
-df = pd.read_csv('data/data.csv')
+# Set page title and favicon
+st.set_page_config(page_title="Homepage", page_icon="assets/EENC-logo.png", layout="wide")
 
-# put logo on sidebar
+# Load the data
+data = pd.read_csv("data/data.csv")
+
+# logo
 st.image("assets/EENC-logo.png", width=100)
 
-# Add a title to the app
-st.title('EENC Educator Support Program Feedback')
+st.title("Welcome to the EENC dashboard!")
+st.markdown("This page displays overall information for the EENC courses. Use the filters on the left to customize the results")
+st.markdown('   ')
 
-# Number of form responses
-num_responses = len(df)
+primary_color = "#195E4C"
+secondary_color = "#3C9E8D"
+text_color = "#6D7183"
 
-# Improvement effect
-improvement_effect = df["Improvement Efforts"].sum()
+# Sidebar
+st.sidebar.title("Filters")
+form_name = st.sidebar.selectbox("Form Name", ['All'] + sorted(data['Form Name'].unique()))
+location = st.sidebar.radio("Location", ['All', 'Online', 'In-Person'])
 
-# Average ratings
-avg_instructor_rating = df["Instructor Rating"].mean()
-avg_accessibility_rating = df["Accessibility Rating"].mean()
-avg_navigation_rating = df["Navigation Rating"].mean()
-avg_rating = 4.76
+# Filter the data
+if form_name != 'All':
+    data = data[data['Form Name'] == form_name]
+if location != 'All':
+    data = data[data['Online/In-Person'] == location]
 
-# Add metric to summarize form numbers
-col1, col2, col3 = st.columns(3)
-col1.metric("Number of form responses", num_responses)
-col2.metric("Improvement Efforts", improvement_effect)
-col3.metric("Average ratings", avg_rating)
+st.markdown('   ')
+col1, col2 = st.columns(2)
+col1.metric("Total Responses", len(data))
+col2.metric("Improvement Efforts", data["Improvement Efforts"].sum())
+st.markdown('   ')
 
-# Divider
-st.markdown('---')
+st.header("Ratings")
+# total number of respondents, course rating, average ratings
+# put some stuff that links to mel's rating page
+col1, col2, col3, col4 = st.columns(4)
+col1.metric("Avg. Course", round(data['Course Rating'].mean(), 2))
+col2.metric("Avg. Instructor", round(data['Instructor Rating'].mean(), 2))
+col3.metric("Avg. Accessibility", round(data['Accessibility Rating'].mean(), 2))
+col4.metric("Avg. Navigation", round(data['Navigation Rating'].mean(), 2))
+st.markdown('[Go to Rating page for more details!](#ratings)')
 
-# Get unique event names
-event_names = df['Form Name'].unique().tolist()
+st.markdown('   ')
+st.markdown('   ')
+st.header("\nGraphs & Trends")
+# Average knowledge gain, most popular classes, trends over time
 
-# Allow the user to select an event to filter by
-selected_event = st.selectbox('Select an event', event_names)
 
-# Filter the data based on the selected event
-filtered_df = df[df['Form Name'] == selected_event]
+st.markdown('### Guidelines Before')
 
-# Histograms of rating distributions
-st.write("### Rating Distributions")
-st.write("Distribution of ratings for Course Rating, Instructor Rating, and Accessibility Rating")
-histogram_cols = st.columns(3)
-for i, col in enumerate(["Course Rating", "Instructor Rating", "Accessibility Rating"]):
-    fig = px.histogram(filtered_df, x=col, nbins=10)
-    with histogram_cols[i]:
-        st.plotly_chart(fig, use_container_width=True)
+value_counts = data['Guidelines Before'].value_counts().reset_index()
+value_counts.columns = ['Guidelines Before', 'Count']
+
+# Define the order of the labels
+labels_order = ["Very Low", "Low", "Average", "High", "Very High"]
+
+bar_color = secondary_color
+
+fig = px.bar(value_counts, x='Guidelines Before', y='Count', text='Count',
+             category_orders={'Guidelines Before': labels_order},
+             color_discrete_sequence=[bar_color])
+fig.update_traces(texttemplate='%{text}', textposition='auto')
+
+fig.update_layout(title="Count of Values in 'Guidelines Before' Column", xaxis_title='Guidelines Before',
+                  yaxis_title='Count')
+
+st.plotly_chart(fig)
+
+
+st.markdown('   ')
+
+st.markdown('### Guidelines After')
+
+value_counts = data['Guidelines After'].value_counts().reset_index()
+value_counts.columns = ['Guidelines After', 'Count']
+
+# Define the order of the labels
+labels_order = ["Average", "High", "Very High"]
+
+bar_color = secondary_color
+
+fig = px.bar(value_counts, x='Guidelines After', y='Count', text='Count',
+             category_orders={'Guidelines After': labels_order},
+             color_discrete_sequence=[bar_color])
+fig.update_traces(texttemplate='%{text}', textposition='auto')
+
+fig.update_layout(title="Count of Values in 'Guidelines After' Column", xaxis_title='Guidelines After',
+                  yaxis_title='Count')
+
+st.plotly_chart(fig)
+
+st.markdown('   ')
+
+st.markdown('### Sharing Interest')
+
+value_counts = data['Sharing Interest'].value_counts().reset_index()
+value_counts.columns = ['Sharing Interest', 'Count']
+
+# Define the order of the labels
+labels_order = ["Average", "High", "Very High"]
+
+bar_color = secondary_color
+
+fig = px.bar(value_counts, x='Sharing Interest', y='Count', text='Count',
+             category_orders={'Sharing Interest': labels_order},
+             color_discrete_sequence=[bar_color])
+fig.update_traces(texttemplate='%{text}', textposition='auto')
+
+fig.update_layout(title="Count of Values in 'Sharing Interest' Column", xaxis_title='Sharing Interest',
+                  yaxis_title='Count')
+
+st.plotly_chart(fig)
+
+# Add CSS to customize text colors
+st.markdown(f"""
+    <style>
+        div.css-edivx2.e16fv1kl3 {{
+            color: {secondary_color};
+        }}
+        p {{
+            color: {text_color};
+        }}
+        h1, h2, h3, h4, h5, h6 {{
+            color: {primary_color};
+        }}
+    </style>
+""", unsafe_allow_html=True)
