f7d7ee4b11ea3b3678ca6ec4845ae020fdf89f25
Meliora Ho <85920674+mellieho9@users.noreply.github.com>
2023-03-23 17:46:35 -0400

    updated shit

---
+++ .streamlit/data.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from google.cloud import firestore
+from google.oauth2 import service_account
+import json
+
+
+# Authenticate to Firestore with the JSON account key.
+key_dict = json.loads(st.secrets['textkey'])
+creds = service_account.Credentials.from_service_account_info(key_dict)
+db = firestore.Client(credentials=creds)
+
+# Create a reference to the Google post.
+doc_ref = db.collection('master_data')
+
